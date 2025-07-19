d3258abe22b2296d192fececdfa2ccbc03585f2f
SiphoVokal <163132466+SiphoVokal@users.noreply.github.com>
2025-07-19 11:38:36 +0200

    Image debugging

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -8,7 +8,7 @@ import Pill from "./Pill"
 const Card: React.FC = () => {
   return (
     <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg ">
-        <img className="rounded-lg" src="{HOUSE_IMAGE}" width={378.56} height={299.37} alt="house image" />
+        <Image className="rounded-lg" src={HOUSE_IMAGE} width={378.56} height={299.37} alt="house image" />
         <div className="p-2 flex gap-2 mt-2">
             <Pill title="Top Villa"/>
             <Pill title="Self CheckIn" />
@@ -20,7 +20,7 @@ const Card: React.FC = () => {
                 <p className=" font-medium text=[17px] text-[#929292]">Sideman, Bali, Indonesia</p>
             </div>
             <div className="flex items-center">
-                <img src="{STAR_IMAGE}" alt="star" />
+                <Image src={STAR_IMAGE} alt="star" />
                 <p className=" font-medium text=[17px] ml-2">4.76</p>
             </div>
         </div>
