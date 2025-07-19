182dc45c88081165d481085fd390cc3cf3c7361d
SiphoVokal <163132466+SiphoVokal@users.noreply.github.com>
2025-07-19 12:19:06 +0200

    Props

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,11 +1,9 @@
-interface CardProps {
-  title: string
-}
+import { PillProps } from "@/interfaces";
 
-const Pill: React.FC<CardProps> = () => {
+const Pill: React.FC<PillProps> = () => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
+      <p className=" text-sm ">title</p>
     </div>
   )
 }
