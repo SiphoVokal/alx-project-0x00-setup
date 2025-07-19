778404c9abaf613b4e9359a16d76e550741451a8
SiphoVokal <163132466+SiphoVokal@users.noreply.github.com>
2025-07-19 13:50:15 +0200

    lg

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -10,21 +10,21 @@ const Landing: React.FC = () => {
       <div className="flex gap-4 m-4">
         <Button title="Small sm" styles="text-sm px-1 py-1 rounded-sm" />
         <Button title="Small md" styles="text-sm px-1 py-1 rounded-md" />
-        <Button title="Small full" styles="text-sm px-1 py-1 rounded-full" />
+        <Button title="Small full" styles="text-sm px-1 py-1 rounded-lg" />
       </div>
 
       {/* Medium Buttons */}
       <div className="flex gap-4 m-4">
         <Button title="Medium sm" styles="text-base px-3 py-1 rounded-sm" />
         <Button title="Medium md" styles="text-base px-3 py-1 rounded-md" />
-        <Button title="Medium full" styles="text-base px-3 py-1 rounded-full" />
+        <Button title="Medium full" styles="text-base px-3 py-1 rounded-lg" />
       </div>
 
       {/* Large Buttons */}
       <div className="flex gap-4">
         <Button title="Large sm" styles="text-lg px-5 py-2 rounded-sm" />
         <Button title="Large md" styles="text-lg px-5 py-2 rounded-md" />
-        <Button title="Large full" styles="text-lg px-5 py-2 rounded-full" />
+        <Button title="Large full" styles="text-lg px-5 py-2 rounded-lg" />
       </div>
     </div>
   )
